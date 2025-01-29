10a761abdada1795dc1a9741ef923fee57fd1b57
Greg Cook <greg.cook@nutrien.com>
2025-01-29 04:35:05 -0700

    xer-1 - added SychronizedOn and IsDeleted to the IAuditable

--- src/Xerris.DotNet.Data/Domain/AuditImmutableBase.cs
+++ src/Xerris.DotNet.Data/Domain/AuditImmutableBase.cs
@@ -3,10 +3,11 @@ namespace Xerris.DotNet.Data.Domain;
 public abstract class AuditImmutableBase : IAuditable, IDeleteable
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public DateTime CreatedOn { get; set; }
     public Guid? CreatedBy { get; set; }
-    public DateTime ModifiedOn { get; set; }
+    public DateTime CreatedOn { get; set; }
     public Guid? ModifiedBy { get; set; }
+    public DateTime ModifiedOn { get; set; }
+    public DateTime SynchronizedOn { get; set; }
     public int Version { get; set; }
     public bool IsDeleted { get; set; }
 }
